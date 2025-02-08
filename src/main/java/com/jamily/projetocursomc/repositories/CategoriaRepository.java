bbf5fad45203048ba0f994affb2ff6acf7917e60
Jamily Barbosa <jamily.barbosa@tgid.com.br>
2025-02-08 19:45:02 -0300

    FEAT: adicionada busca em repository para primeira estrutura de testes unitários

--- src/main/java/com/jamily/projetocursomc/repositories/CategoriaRepository.java
+++ src/main/java/com/jamily/projetocursomc/repositories/CategoriaRepository.java
@@ -1,5 +1,7 @@
 package com.jamily.projetocursomc.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.jamily.projetocursomc.domain.Categoria;
 
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
-
+    Optional <Categoria> findByNome(String name);
 }
